810a05fd3b65bede66d98615828e738eaa8fbbf5
Rafael Chioca <rafaelmchioca@gmail.com>
2023-04-25 00:15:37 +0000

    (27/03)

--- back-end/controllers/sale.js
+++ back-end/controllers/sale.js
@@ -19,69 +19,104 @@ controller.create = async (req, res) => {
   }
 }
 
-controller.retrieve = async (req, res) => {
-  try{
-
-  }
-  catch(error) {
-    console.error(error)
-    // TTP 500: Internal Server Error
-    res.status(500).send(error)
-  }
-}
-
 controller.retrieveAll = async (req, res) => {
-  try{
-    // Retonra todos os documentos da coleção
-    const result = await Sale.find().populate('customer').populate('items.product')
-    // HTTP 200: Ok (implicito)
+  try {
+    // Retorna todos os documentos da coleção
+    const result = await Sale.find()
+      .populate('customer')
+      .populate('items.product')
+
+    // HTTP 200: OK (implícito)
     res.send(result)
   }
   catch(error) {
     console.error(error)
-    // TTP 500: Internal Server Error
+    // HTTP 500: Internal Server Error
     res.status(500).send(error)
   }
 }
 
-controller.retrieveOne = async (req, res) => {
-  try{
-    // Retonra todos os documentos da coleção
-    const result = await Sale.findById(req.params.id).populate('customer').populate('items.product')
+controller.retrieveOne = async(req, res) => {
+  try {
+    const result = await Sale.findById(req.params.id)
+      .populate('customer')
+      .populate('items.product')
 
     if(result) {
-      // Encontrou o documento -> HTTP 200 OK (Implicito)
+      // Encontrou o documento ~> HTTP 200: OK (implícito)
       res.send(result)
     }
     else {
-      // Não encontrou o documento
-      res.status(404)
+      // Não encontrou o documento ~> HTTP 404: Not found
+      res.status(404).end()
     }
-    // HTTP 200: Ok (implicito)
   }
   catch(error) {
     console.error(error)
-    // TTP 500: Internal Server Error
+    // HTTP 500: Internal Server Error
     res.status(500).send(error)
   }
 }
 
 controller.update = async (req, res) => {
   try {
-      const result = await Sale.findByIdAndUpdate(req.params.id, req.body)
 
-      if(result) {
-        // Entrou e atualizou -> HTTP 204: No content
-        res.status(204).end()
+    // Encontra a venda para ser atualizada
+    const sale = await Sale.findById(req.params.id)
+    
+    // items foi passado em req.body
+    if(req.body.items) {
+      // Percorre cada item de req.body, verificando se já existe
+      // ou não em sales.item
+      for(let item of req.body.items) {
+        // Se o item tem _id, é porque já existe ~> É CASO DE ATUALIZAÇÃO
+        if(item._id) {
+
+          // Verifica se foi passada uma propriedade especial chamada "$_delete", com valor true e nesse caso deleta o subdocumento
+          if(item["$_delete"] === true) {
+            sale.items.id(item._id).deleteOne()
+          }
+          else{
+            // Procura cada propriedade no item de req.body e atualiza no documento
+            for(let prop in item) {
+              sale.items.id(item._id)[prop] = item[prop]
+            }
+          }        
+        }
+        // Item não existe ~> É caso de inserção
+        else {
+          sale.items.push(item)   // Cria um novo item
+        }
       }
-      else {
-        // Não encontrou para atualziar -> HTTP 404: Not found
-        res.status(404).end()
+
+      // Indica que o items foi modificado e deve ser regravado
+      sale.markModified('items')
+
+    }
+
+    // Verifica as demais propriedades do pai (sale) por alterações
+    for(let prop in req.body) {
+      if(prop !== 'items') {  // Items já foi processado acima
+        console.log({prop})
+        sale[prop] = req.body[prop]
+        sale.markModified(prop)
       }
+    }
+    
+    const result = await sale.save()
+
+    if(result) {
+      // Encontrou e atualizou ~> HTTP 204: No content
+      res.status(204).end()
+    }
+    else {
+      // Não encontrou para atualizar ~> HTTP 404: Not found
+      res.status(404).end()
+    }
   }
   catch(error) {
     console.error(error)
-    // TTP 500: Internal Server Error
+    // HTTP 500: Internal Server Error
     res.status(500).send(error)
   }
 }
@@ -91,17 +126,17 @@ controller.delete = async (req, res) => {
     const result = await Sale.findByIdAndDelete(req.params.id)
 
     if(result) {
-      // Encontrou e. excluiu -> HTTP 204: No content
-      res.status(204).end
+      // Encontrou e excluiu ~> HTTP 204: No content
+      res.status(204).end()
     }
     else {
-      // Não encontrou para excluir -> HTTP 04: Not found
+      // Não encontrou para excluir ~> HTTP 404: Not found
       res.status(404).end()
     }
   }
   catch(error) {
     console.error(error)
-    // TTP 500: Internal Server Error
+    // HTTP 500: Internal Server Error
     res.status(500).send(error)
   }
 }
